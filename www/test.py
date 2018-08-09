4b99a312c3faf413eb8669beebbc664428ad5d5f
longzhijun <longzhijun@meitunmama.com>
2018-08-09 09:33:02 +0800

    note

--- www/test.py
+++ www/test.py
@@ -24,4 +24,14 @@ def test():
 
 
 loop = asyncio.get_event_loop()
-# loop.run_until_complete(test())
+loop.run_until_complete(test())
+
+from wsgiref.simple_server import make_server
+# 导入我们自己编写的application函数:
+
+
+# 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
+httpd = make_server('', 8000, '')
+print('Serving HTTP on port 8000...')
+# 开始监听HTTP请求:
+httpd.serve_forever()
